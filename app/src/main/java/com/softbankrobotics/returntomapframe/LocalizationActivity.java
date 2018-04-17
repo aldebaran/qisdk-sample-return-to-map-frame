d1b6c019a0f7a013f34ac96abc7873af493ae7ab
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-04-17 17:30:48 +0200

    Make LocalizationActivity implement RobotLifecycleCallbacks

--- app/src/main/java/com/softbankrobotics/returntomapframe/LocalizationActivity.java
+++ app/src/main/java/com/softbankrobotics/returntomapframe/LocalizationActivity.java
@@ -2,12 +2,42 @@ package com.softbankrobotics.returntomapframe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class LocalizationActivity extends AppCompatActivity {
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+
+public class LocalizationActivity extends AppCompatActivity implements RobotLifecycleCallbacks {
+
+    private static final String TAG = "LocalizationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localization);
+
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        // TODO: impl
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+        // TODO: impl
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        Log.e(TAG, "onRobotFocusRefused: " + reason);
     }
 }
