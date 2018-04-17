3d89907675d8ba350d8b2353964af78ec7c6cb86
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-04-17 11:15:58 +0200

    Basic UI for menu

--- app/src/main/java/com/softbankrobotics/returntomapframe/MenuActivity.java
+++ app/src/main/java/com/softbankrobotics/returntomapframe/MenuActivity.java
@@ -2,12 +2,33 @@ package com.softbankrobotics.returntomapframe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MenuActivity extends AppCompatActivity {
+
+    @BindView(R.id.useMapButton)
+    Button useMapButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+
+        useMapButton.setEnabled(false);
+    }
+
+    @OnClick(R.id.createMapButton)
+    public void onClickCreateMap() {
+        // TODO: impl
+    }
+
+    @OnClick(R.id.useMapButton)
+    public void onClickUseMap() {
+        // TODO: impl
     }
 }
