ef386f48fd3c2406277da9ed58d66682acd27222
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-04-18 09:50:33 +0200

    Perform localization

--- app/src/main/java/com/softbankrobotics/returntomapframe/LocalizationActivity.java
+++ app/src/main/java/com/softbankrobotics/returntomapframe/LocalizationActivity.java
@@ -1,5 +1,6 @@
 package com.softbankrobotics.returntomapframe;
 
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,15 +8,26 @@ import android.util.Log;
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.object.actuation.LocalizationStatus;
+import com.aldebaran.qi.sdk.object.actuation.Localize;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LocalizationActivity extends AppCompatActivity implements RobotLifecycleCallbacks {
 
     private static final String TAG = "LocalizationActivity";
 
+    @Nullable
+    private QiContext qiContext;
+    @Nullable
+    private Localize localize;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_localization);
+        ButterKnife.bind(this);
 
         QiSDK.register(this, this);
     }
@@ -28,16 +40,58 @@ public class LocalizationActivity extends AppCompatActivity implements RobotLife
 
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
-        // TODO: impl
+        this.qiContext = qiContext;
     }
 
     @Override
     public void onRobotFocusLost() {
-        // TODO: impl
+        this.qiContext = null;
     }
 
     @Override
     public void onRobotFocusRefused(String reason) {
         Log.e(TAG, "onRobotFocusRefused: " + reason);
+    }
+
+    @OnClick(R.id.startLocalizationButton)
+    public void onClickStartLocalization() {
+        startLocalization();
+    }
+
+    private void startLocalization() {
+        if (qiContext == null) {
+            Log.e(TAG, "Error while localizing: qiContext is null");
+            return;
+        }
+
+        Log.d(TAG, "Retrieving map...");
+        MapManager.getInstance().retrieveMap(qiContext)
+                .andThenCompose(map -> {
+                    Log.d(TAG, "Map retrieved");
+                    Log.d(TAG, "Building Localize...");
+                    return qiContext.getMapping().async().makeLocalize(qiContext.getRobotContext(), map);
+                })
+                .andThenCompose(loc -> {
+                    Log.d(TAG, "Localize built");
+
+                    localize = loc;
+
+                    localize.setOnStatusChangedListener(status -> {
+                        if (status == LocalizationStatus.LOCALIZED) {
+                            Log.d(TAG, "Robot is localized");
+                        }
+                    });
+
+                    return localize.async().run();
+                })
+                .thenConsume(future -> {
+                    if (localize != null) {
+                        localize.setOnStatusChangedListener(null);
+                    }
+
+                    if (future.hasError()) {
+                        Log.e(TAG, "Error while localizing", future.getError());
+                    }
+                });
     }
 }
--- app/src/main/java/com/softbankrobotics/returntomapframe/MapManager.java
+++ app/src/main/java/com/softbankrobotics/returntomapframe/MapManager.java
@@ -5,6 +5,8 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.aldebaran.qi.Future;
+import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.object.actuation.ExplorationMap;
 import com.snatik.storage.Storage;
 
@@ -45,12 +47,33 @@ class MapManager {
         }
 
         Storage storage = new Storage(context);
-        return storage.isFileExist(storage.getInternalFilesDirectory() + "/" + MAP_FILENAME);
+        return storage.isFileExist(mapFilePath(storage));
+    }
+
+    @NonNull
+    Future<ExplorationMap> retrieveMap(@NonNull QiContext qiContext) {
+        if (explorationMap != null) {
+            return Future.of(explorationMap);
+        }
+
+        Storage storage = new Storage(qiContext);
+        String data = storage.readTextFile(mapFilePath(storage));
+
+        return qiContext.getMapping().async().makeMap(data)
+                .andThenApply(map -> {
+                    explorationMap = map;
+                    return explorationMap;
+                });
     }
 
     private void writeMapToFile(@NonNull Context context, @NonNull String data) {
         Storage storage = new Storage(context);
-        storage.createFile(storage.getInternalFilesDirectory() + "/" + MAP_FILENAME, data);
+        storage.createFile(mapFilePath(storage), data);
+    }
+
+    @NonNull
+    private String mapFilePath(@NonNull Storage storage) {
+        return storage.getInternalFilesDirectory() + "/" + MAP_FILENAME;
     }
 
     private static final class Holder {
