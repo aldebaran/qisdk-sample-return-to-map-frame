64eaa348cbb984081ceb43c3204ba87a8f564a1d
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-07-30 16:46:09 +0200

    Add empty IntroductionActivity

---
+++ app/src/main/java/com/softbankrobotics/sample/returntomapframe/introduction/IntroductionActivity.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2018 Softbank Robotics Europe
+ * See COPYING for the license
+ */
+
+package com.softbankrobotics.sample.returntomapframe.introduction;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.softbankrobotics.sample.returntomapframe.R;
+
+public class IntroductionActivity extends RobotActivity implements RobotLifecycleCallbacks {
+
+    private static final String TAG = "IntroductionActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_introduction);
+
+        QiSDK.register(this, this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        Log.e(TAG, "onRobotFocusRefused: " + reason);
+    }
+}
