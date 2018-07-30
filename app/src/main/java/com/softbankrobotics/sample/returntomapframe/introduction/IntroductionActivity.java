fad8951170dbef68bb41d605c825bbf212c50e60
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-07-30 18:45:38 +0200

    Intro - Add speech and go to menu

--- app/src/main/java/com/softbankrobotics/sample/returntomapframe/introduction/IntroductionActivity.java
+++ app/src/main/java/com/softbankrobotics/sample/returntomapframe/introduction/IntroductionActivity.java
@@ -5,15 +5,19 @@
 
 package com.softbankrobotics.sample.returntomapframe.introduction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.aldebaran.qi.sdk.QiContext;
 import com.aldebaran.qi.sdk.QiSDK;
 import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
 import com.aldebaran.qi.sdk.design.activity.conversationstatus.SpeechBarDisplayStrategy;
+import com.aldebaran.qi.sdk.object.conversation.Say;
 import com.softbankrobotics.sample.returntomapframe.R;
+import com.softbankrobotics.sample.returntomapframe.menu.MenuActivity;
 
 public class IntroductionActivity extends RobotActivity implements RobotLifecycleCallbacks {
 
@@ -36,16 +40,29 @@ public class IntroductionActivity extends RobotActivity implements RobotLifecycl
 
     @Override
     public void onRobotFocusGained(QiContext qiContext) {
+        Say say = SayBuilder.with(qiContext)
+                .withResource(R.string.intro_speech)
+                .build();
 
+        say.async().run()
+                .andThenConsume(ignored -> goToMenu());
     }
 
     @Override
     public void onRobotFocusLost() {
-
+        // Not used.
     }
 
     @Override
     public void onRobotFocusRefused(String reason) {
         Log.e(TAG, "onRobotFocusRefused: " + reason);
+    }
+
+    private void goToMenu() {
+        runOnUiThread(() -> {
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
