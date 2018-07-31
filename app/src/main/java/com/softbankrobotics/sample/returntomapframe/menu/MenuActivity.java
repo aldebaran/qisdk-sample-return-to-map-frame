70996f88b3fc4adf88c8cef62e540b77981123d3
Adrien Kvaternik <adrien.kvaternik@gmail.com>
2018-07-31 15:10:16 +0200

    Menu - Implement robot lifecycle callbacks

--- app/src/main/java/com/softbankrobotics/sample/returntomapframe/menu/MenuActivity.java
+++ app/src/main/java/com/softbankrobotics/sample/returntomapframe/menu/MenuActivity.java
@@ -7,9 +7,13 @@ package com.softbankrobotics.sample.returntomapframe.menu;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
 import com.aldebaran.qi.sdk.design.activity.RobotActivity;
 import com.softbankrobotics.sample.returntomapframe.R;
 import com.softbankrobotics.sample.returntomapframe.core.MapManager;
@@ -20,7 +24,9 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MenuActivity extends RobotActivity {
+public class MenuActivity extends RobotActivity implements RobotLifecycleCallbacks {
+
+    private static final String TAG = "MenuActivity";
 
     @BindView(R.id.useMapButton)
     Button useMapButton;
@@ -31,6 +37,8 @@ public class MenuActivity extends RobotActivity {
 
         setContentView(R.layout.activity_menu);
         ButterKnife.bind(this);
+
+        QiSDK.register(this, this);
     }
 
     @Override
@@ -42,6 +50,27 @@ public class MenuActivity extends RobotActivity {
         if (MapManager.getInstance().hasMap(getApplicationContext())) {
             useMapButton.setEnabled(true);
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        QiSDK.unregister(this, this);
+        super.onDestroy();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+        Log.e(TAG, "onRobotFocusRefused: " + reason);
     }
 
     @OnClick(R.id.createMapButton)
